78b312f9564599df0cc3411ff96a1bc8fd341033
Bilel AIT ALLA <bilel.aitalla66@gmail.com>
2022-05-16 10:21:40 +0200

    update avec gitignore

--- src/ghettorraria/Main.java
+++ src/ghettorraria/Main.java
@@ -3,11 +3,8 @@ package ghettorraria;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Control;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class Main extends Application {
 	
